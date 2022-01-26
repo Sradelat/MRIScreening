7d5761ac15a51581ab95812e8c79dd40af0de1fa
Sradelat <shawnradelat@gmail.com>
2022-01-25 20:58:41 -0500

    + function to check answers + function to store card info + follow-up questions + comments

--- MyTools.py
+++ MyTools.py
@@ -1,43 +1,61 @@
 from datetime import date
 
 
-def current_age_calculator(dob):
-    dob_month = int(dob.split("/")[0])
+def current_age_calculator(dob):  # uses datetime for real time age calculation
+    dob_month = int(dob.split("/")[0])  # splitting days months years correctly for calculation below
     dob_day = int(dob.split("/")[1])
     dob_year = int(dob.split("/")[2])
     current_date = str(date.today())  # pull current date and convert to str
     current_month = int(current_date.split("-")[1])
     current_day = int(current_date.split("-")[2])
     current_year = int(current_date.split("-")[0])
-    if (dob_month + dob_day) > (current_month + current_day):
-        current_age = current_year - dob_year - 1
+    if (dob_month + dob_day) > (current_month + current_day):  # checks to see if birthday has happened yet this year
+        current_age = current_year - dob_year - 1  # if birthday hasn't happened then subtract a year
     else:
-        current_age = current_year - dob_year
+        current_age = current_year - dob_year  # if birthday happened this year
     return current_age
 
 
-def appt_age_calculator(appt, dob):
-    dob_month = int(dob.split("/")[0])
+def appt_age_calculator(appt, dob):  # Calculates patient's age at appointment date to see if patient needs labs
+    dob_month = int(dob.split("/")[0])  # splitting days months years correctly for calculation below
     dob_day = int(dob.split("/")[1])
     dob_year = int(dob.split("/")[2])
     appt_month = int(appt.split("/")[0])
     appt_day = int(appt.split("/")[1])
     appt_year = int(appt.split("/")[2])
-    if (dob_month + dob_day) > (appt_month + appt_day):
-        appt_age = appt_year - dob_year - 1
+    if (dob_month + dob_day) > (appt_month + appt_day):  # checks to see if birthday has happened yet this year
+        appt_age = appt_year - dob_year - 1  # if birthday hasn't happened then subtract a year
     else:
-        appt_age = appt_year - dob_year
+        appt_age = appt_year - dob_year  # if birthday happened this year
     return appt_age
 
 
-def metric_uscs_height(height):
+def metric_uscs_height(height):  # Converts Metric cm to USCS ft/in
     uscs_feet = int((int(height) * .393701) / 12)  # dropping the remainder with int
     uscs_inches = round((int(height) * .393701) % 12)  # rounding the remainder
-    str_uscs_height = f"{uscs_feet}'{uscs_inches}"
+    str_uscs_height = f"{uscs_feet}'{uscs_inches}"  # makes it pretty
     return str_uscs_height
 
 
-def metric_uscs_weight(weight):
+def metric_uscs_weight(weight):  # Converts Metric KG to USCS lbs
     uscs_weight = round(int(weight) * 2.2)  # round the decimal off
-    str_uscs_weight = f"{uscs_weight}lbs"
+    str_uscs_weight = f"{uscs_weight}lbs"  # makes it pretty
     return str_uscs_weight
+
+
+def valid_answer():  # checking to see if answer to question was y or n - otherwise throw error
+    while True:  # loop until satisfied
+        answer = input(">").lower()  # not case-sensitive
+        if answer == "y" or answer == "n":  # asking for y or n
+            break  # satisfied
+        else:
+            print("Invalid entry. Please enter 'y' or 'n'.")  # if any other input happens they get this error
+    return answer
+
+# Don't want to combine valid_answer and check_card because it could force useless input on patient :(
+
+
+def check_card():  # if the patient has an implant card they can input the info here
+    print("Please enter card information if available. If you do not have a card describe the implant: ")
+    card_info = input(">")  # this input will be sent to technologist for further research
+    return card_info
--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import MyTools
 
 # If Q = yes - prompt technologist with each instance
 # If no go - prompt staff
-# Dictionary to pull specifics?
+# Dictionary to pull specifics? -> can't use input fx in dict
 # age can determine if minor or not
-# make pt confirm demographics are correct
 # ask for phone number for tech to reach or pull it from their file?
+# create schedule file next?
 
 appt = "07/20/2022"  # going to pull from file
 while True:
@@ -20,7 +20,7 @@ while True:
         else:
             print("Invalid answer. Please enter male or female.")
     # HEIGHT WEIGHT
-    while True:
+    while True:  # might use valid_answer fx to simplify these loops
         m_units = input("Would you like to use the metric system for measurements? (height in cm and weight in kg) "
                         "[y/n] ")
         # Metric system
@@ -82,6 +82,139 @@ while True:
     else:
         continue
 
+# SETTINGS - need a hub to customize settings such as this
+scan_pacemakers = True
+
+# QUESTIONS - not sure if questions should be added to a list to iterate through. I want questions to be customizable.
+# Perhaps add a clause at the end for custom questions the admin user wants to add in
+print("Is there ANY chance you could be pregnant? [y/n] ")
+pregnant = MyTools.valid_answer()
+
+print("Do you currently have a pacemaker/defibrillator? [y/n] ")
+pacemaker = MyTools.valid_answer()
+if pacemaker == "y":
+    if scan_pacemakers:
+        pacemaker_info = MyTools.check_card()
+    if not scan_pacemakers:
+        input("We can not scan pacemakers at this location. Please inform staff. Press enter to exit.")
+        quit()
+
+print("Have you ever had a pacemaker/defibrillator? [y/n] ")
+past_pacemaker = MyTools.valid_answer()
+if past_pacemaker == "y":
+    wires = input("Do you have abandoned pacemaker wires still in place?")
+    if wires == "y":
+        wires_info = MyTools.check_card()
+
+print("Do you have a brain aneurysm clip? [y/n] ")
+clip = MyTools.valid_answer()
+if clip == "y":
+    clip_info = MyTools.check_card()
+
+print("Do you have a nerve or bone growth stimulator? [y/n] ")
+stimulator = MyTools.valid_answer()
+if stimulator == "y":
+    stimulator_info = MyTools.check_card()
+
+print("Do you have any stents? [y/n] ")
+stents = MyTools.valid_answer()
+if stents == "y":
+    stents_info = MyTools.check_card()
+
+print("Do you have any intravascular coils? [y/n] ")
+coils = MyTools.valid_answer()
+if coils == "y":
+    coils_info = MyTools.check_card()
+
+print("Do you have any vascular filters? [y/n] ")
+filters = MyTools.valid_answer()
+if filters == "y":
+    filters_info = MyTools.check_card()
+
+print("Do you have an artificial heart valve? [y/n] ")
+valves = MyTools.valid_answer()
+if valves == "y":
+    valves_info = MyTools.check_card()
+
+print("Do you have a shunt? [y/n] ")
+shunt = MyTools.valid_answer()
+if shunt == "y":
+    shunt_info = MyTools.check_card()
+
+print("Do you have any eye implants? [y/n] ")
+eyes = MyTools.valid_answer()
+if eyes == "y":
+    eyes_info = MyTools.check_card()
+
+print("Have you ever worked as a welder or metal shaver? [y/n] ")
+eyes_occupation = MyTools.valid_answer()
+
+print("Do you now or have you ever had an injury involving metal to your eye? [y/n] ")
+metal_in_eyes = MyTools.valid_answer()
+if metal_in_eyes == "y":
+    print("Did you have the metal removed from your eye by a doctor? [y/n] ")
+    metal_in_eyes_removed = MyTools.valid_answer()
+
+print("Do you have any shrapnel, BB's, or gunshot wounds? [y/n] ")
+foreign_body = MyTools.valid_answer()
+
+print("Do you have any ear implants? [y/n] ")
+ears = MyTools.valid_answer()
+if ears == "y":
+    ears_info = MyTools.check_card()
+
+print("Do you wear hearing aids? [y/n] ")
+hearing_aids = MyTools.valid_answer()
+if hearing_aids == "y":
+    print("Your hearing aids will need to be removed for your MRI.")
+
+print("Do you have an implanted drug pump? [y/n] ")
+drug_pump = MyTools.valid_answer()
+if drug_pump == "y":
+    drug_pump_info = MyTools.check_card()
+
+print("Do you have an insulin pump? [y/n] ")
+insulin_pump = MyTools.valid_answer()
+if insulin_pump == "y":
+    print("Your insulin pump will need to be removed for your MRI. Please plan accordingly.")
+
+print("Any other metallic implants in your body? [y/n] ")
+metallic_implants = MyTools.valid_answer()
+if metallic_implants == "y":
+    print("Please type in any metallic implants in your body that we have not asked about: ")
+    metallic_implants_info = input(">")
+
+# Is exam with contrast?
+print("Have you ever had MRI contrast before? [y/n] ")  # have you had a reaction?
+contrast = MyTools.valid_answer()
+if contrast == "y":
+    print("Did your body have a negative reaction to the MRI contrast?")
+    reaction = MyTools.valid_answer()
+
+print("Are you diabetic? [y/n] ")
+diabetic = MyTools.valid_answer()
+
+print("Do you have a history of high blood pressure? [y/n] ")
+blood_pressure = MyTools.valid_answer()
+
+print("Do you have a history of kidney failure? [y/n] ")
+kidneys = MyTools.valid_answer()
+
+print("Are you on dialysis? [y/n] ")
+dialysis = MyTools.valid_answer()
+
+print("Do you have any liver disease? [y/n] ")
+liver_disease = MyTools.valid_answer()
+
+print("Do you have multiple myeloma? [y/n] ")
+multiple_myeloma = MyTools.valid_answer()
+
+
+
+
+
+
+# # COPY
 # pregnant = input("Is there ANY chance you could be pregnant? [y/n] ")
 # pacemaker = input("Do you currently have a pacemaker/defibrillator? [y/n] ")
 # past_pacemaker = input("Have you ever had a pacemaker/defibrillator? [y/n] ")
@@ -109,4 +242,3 @@ while True:
 # dialysis = input("Are you on dialysis? [y/n] ")
 # liver_disease = input("Do you have any liver disease? [y/n] ")
 # multiple_myeloma = input("Do you have multiple myeloma? [y/n] ")
-
